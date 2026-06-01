b8c6401c50132530436fd61d87c8c30246c4d2cd
Laurence Cadag <laurencecadag1@gmail.com>
2026-06-02 00:02:49 +0800

    Prep for Deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,10 +9,11 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
+      forbidNonWhitelisted: true,
+    }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 10000;
+  await app.listen(port, "0.0.0.0");
 }
 
 bootstrap();
